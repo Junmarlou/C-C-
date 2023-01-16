4adb83e45ea4ee112362a0e2db9447e9abbbda50
Fierrez <114709646+Fierrez@users.noreply.github.com>
2023-01-16 14:30:28 +0800

    Add files via upload

---
+++ C-Group-Live-Coding/problem14.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main (){
+   int salesman1,salesman2,salesman3;
+   
+   cout << "Enter sales for salesman 1" <<endl;
+
+   for(int x; x <= 5; x++){
+      cin >> salesman1[x];
+   }
+
+
+   cout << "JCM ENTERPRISE"<< endl;
+   cout << "TOTAL SALES FOR THE MONTH OF JULY"<< endl;
+
+   cout << "SALESMAN 1:" << endl;
+   cout << "SALESMAN 2:" << endl;
+   cout << "SALESMAN 3:" << endl;
+   cout << "            --------------" << endl;
+   cout << "TOTAL:   " << endl;
+   cout << "SALESMAN WITH HIGHEST SALES: " << endl;
+}
---
+++ C-Group-Live-Coding/problem7.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double grosspay,wholding,rate,hpr,hours,netpay;
+
+    
+
+    cout << "Enter your hourly pay rate:";
+    cin >> hpr;
+    cout << "Enter hours worked:";
+    cin >> hours;
+
+     grosspay=hpr*hours;
+
+    if(grosspay <= 2000){
+        rate=grosspay*0.10;
+        wholding=10; 
+        netpay=grosspay-rate;
+    }
+    if(grosspay >= 2001 &&  grosspay <= 4000){
+        
+        rate=grosspay*0.12;
+        wholding=12; 
+        netpay=grosspay-rate;
+    }
+    if(grosspay >= 4001 && grosspay <= 10000){
+       rate=grosspay*0.15;
+       wholding=15; 
+        netpay=grosspay-rate;
+    }
+    if(grosspay > 10001 ){
+        rate=grosspay*0.20;
+        wholding=20; 
+        netpay=grosspay-rate;
+    
+    }
+    cout << "Your grosspay is " << grosspay << endl;
+    cout << "Your withholding tax is " << wholding <<"%" << endl;
+    cout << "Your rate is " << rate << endl;
+    cout << "Your salary is " << netpay <<  endl;
+}
